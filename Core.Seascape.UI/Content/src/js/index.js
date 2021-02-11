89a53e574913356eb06bf69c838637a3d1b8ebc1
JayJayson84 <jayjayson84@bitbucket.org>
2021-02-11 22:04:05 +0000

    Add SCSS and JS scripts

---
+++ Core.Seascape.UI/Content/src/js/index.js
@@ -0,0 +1,15 @@
+﻿import "core-js/stable";
+
+class App {
+    constructor() {
+        this.modules = [];
+
+        document.documentElement.className = 'js';
+
+        this.modules.forEach(module => {
+            module.init();
+        });
+    }
+}
+
+window.addEventListener('load', () => new App());
