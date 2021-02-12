82d93023d7c1f3af30961adb22d59360908cc11b
JayJayson84 <jayjayson84@bitbucket.org>
2021-02-12 23:30:41 +0000

    Add enums for ajax form attributes

---
+++ Core.Seascape.UI/Enums/AjaxFormIds.cs
@@ -0,0 +1,12 @@
+﻿using Core.Seascape.UI.Helpers.Attributes;
+
+namespace Core.Seascape.UI.Enums
+{
+    /// <summary>
+    /// An enumeration containing unique ajax form id's used throughout the project.
+    /// </summary>
+    public enum AjaxFormIds
+    {
+        [EnumText("seascape-form")] SeascapeFormId
+    }
+}
---
+++ Core.Seascape.UI/Enums/AjaxMode.cs
@@ -0,0 +1,14 @@
+﻿using Core.Seascape.UI.Helpers.Attributes;
+
+namespace Core.Seascape.UI.Enums
+{
+    /// <summary>
+    /// An enumeration containing ajax form modes.
+    /// </summary>
+    public enum AjaxMode
+    {
+        [EnumText("replace")] Replace,
+        [EnumText("before")] Before,
+        [EnumText("after")] After
+    }
+}
