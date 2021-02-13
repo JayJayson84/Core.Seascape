e04515a98cf3fc048762833d1e7c64d0a5b119d7
JayJayson84 <jayjayson84@bitbucket.org>
2021-02-13 21:19:43 +0000

    Add DataAnnotations for form attributes

---
+++ Core.Seascape.UI/Helpers/DataAnnotations/StepAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace System.ComponentModel.ExtendedDataAnnotations
+{
+    public class StepAttribute : ValidationAttribute
+    {
+        public int Step { get; set; }
+
+        public StepAttribute(int value)
+        {
+            Step = value;
+        }
+
+        public override bool IsValid(object value)
+        {
+            return value is int;
+        }
+    }
+}
